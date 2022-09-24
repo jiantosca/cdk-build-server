c477ebc2a3c1e6870e4b81030f1eaa88756bfcd3
jiantosca <jiantosca@gmail.com>
2022-09-24 18:35:42 -0400

    setting up spotless

--- src/main/java/com/jiantosca/CdkBuildServerApp.java
+++ src/main/java/com/jiantosca/CdkBuildServerApp.java
@@ -1,42 +1,36 @@
 package com.jiantosca;
 
 import software.amazon.awscdk.App;
-import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class CdkBuildServerApp {
-    public static void main(final String[] args) {
-        App app = new App();
+	public static void main(final String[] args) {
+		App app = new App();
 
-        new CdkBuildServerStack(app, "CdkBuildServerStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+		new CdkBuildServerStack(app, "CdkBuildServerStack", StackProps.builder().description(
+				"This stack creates a Jenkins based build server with Sonar, Docker, git, and node installed on it.")
+				// If you don't specify 'env', this stack will be environment-agnostic.
+				// Account/Region-dependent features and context lookups will not work,
+				// but a single synthesized template can be deployed anywhere.
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+				// Uncomment the next block to specialize this stack for the AWS Account
+				// and Region that are implied by the current CLI configuration.
+				/*
+				 * .env(Environment.builder() .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+				 * .region(System.getenv("CDK_DEFAULT_REGION")) .build())
+				 */
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
+				// Uncomment the next block if you know exactly what Account and Region you
+				// want to deploy the stack to.
+				/*
+				 * .env(Environment.builder() .account("123456789012") .region("us-east-1")
+				 * .build())
+				 */
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+				// For more information, see
+				// https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+				.build());
 
-        app.synth();
-    }
+		app.synth();
+	}
 }
-
--- src/main/java/com/jiantosca/CdkBuildServerStack.java
+++ src/main/java/com/jiantosca/CdkBuildServerStack.java
@@ -1,24 +1,24 @@
 package com.jiantosca;
 
-import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.constructs.Construct;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 
 public class CdkBuildServerStack extends Stack {
-    public CdkBuildServerStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
+	public CdkBuildServerStack(final Construct scope, final String id) {
+		this(scope, id, null);
+	}
 
-    public CdkBuildServerStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+	public CdkBuildServerStack(final Construct scope, final String id, final StackProps props) {
+		super(scope, id, props);
 
-        // The code that defines your stack goes here
+		// The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "CdkBuildServerQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
-    }
+		// example resource
+		// final Queue queue = Queue.Builder.create(this, "CdkBuildServerQueue")
+		// .visibilityTimeout(Duration.seconds(300))
+		// .build();
+	}
 }
